969f8528f415e1fa7f3ee5bfb8de0e6a0c482ca0
Ryan <stander.ryan@gmail.com>
2024-02-28 18:58:29 +0100

    Add event manager
    I just think theyre neat

---
+++ Assets/Scripts/Events/EventDataInheritances.cs
@@ -0,0 +1,93 @@
+﻿//Event that informs subscribers of a debug log
+
+using Crew;
+using TimeAndSeasons;
+using Weather;
+
+namespace Events
+{
+    public class DateChange : EventData
+    {
+        public readonly int CurrentDay;
+        public readonly int CurrentMonth;
+        public readonly Season CurrentSeason;
+        public readonly int CurrentYear;
+
+        public DateChange(int currentDay, int currentMonth, Season currentSeason, int currentYear) : base(
+            EventIdentifiers.DateChange)
+        {
+            CurrentDay = currentDay;
+            CurrentMonth = currentMonth;
+            CurrentSeason = currentSeason;
+            CurrentYear = currentYear;
+        }
+    }
+
+    public class SendTimeStrength : EventData
+    {
+        public readonly float TimeStrength;
+        public SendTimeStrength(float timeStrength) : base(EventIdentifiers.SendTimeStrength)
+        {
+            TimeStrength = timeStrength;
+        }
+    }
+
+    public class NewDay : EventData
+    {
+        public NewDay():base(EventIdentifiers.NewDay)
+        {
+
+        }
+    }
+
+    public class WeatherHasChanged : EventData
+    {
+        public readonly WeatherPreset WeatherPreset;
+        public WeatherHasChanged(WeatherPreset weatherPreset) : base(EventIdentifiers.WeatherHasChanged)
+        {
+            WeatherPreset = weatherPreset;
+        }
+    }
+
+    public class NewWind : EventData
+    {
+        public readonly float WindSpeed;
+        public readonly float WindDirection;
+        public NewWind(float windSpeed, float windDirection) : base(EventIdentifiers.NewWind)
+        {
+            WindSpeed = windSpeed;
+            WindDirection = windDirection;
+        }
+    }
+
+    #region Crew
+
+    public class RecruitCrewMember : EventData
+    {
+        public readonly CrewMemberStats CrewMemberStats;
+        public RecruitCrewMember(CrewMemberStats crewMemberStats) : base(EventIdentifiers.RecruitCrewMember)
+        {
+            CrewMemberStats = crewMemberStats;
+        }
+    }
+
+    public class SortCrewMember : EventData
+    {
+        public readonly CrewMemberStats CrewMemberStats;
+        public SortCrewMember(CrewMemberStats crewMemberStats) : base(EventIdentifiers.SortCrewMember)
+        {
+            CrewMemberStats = crewMemberStats;
+        }
+    }
+
+    public class RecalculatePlayerCrewModifiers : EventData
+    {
+        public RecalculatePlayerCrewModifiers() : base(EventIdentifiers.RecalculatePlayerCrewModifiers)
+        {
+        }
+    }
+
+    #endregion
+
+
+}
---
+++ Assets/Scripts/Events/EventIdentifiers.cs
@@ -0,0 +1,15 @@
+﻿//Defines the different event types to be used in event data in enumeration form
+namespace Events
+{
+    public enum EventIdentifiers
+    {
+        DateChange=0,
+        SendTimeStrength=1,
+        NewDay=2,
+        WeatherHasChanged=3,
+        NewWind=4,
+        RecruitCrewMember=5,
+        SortCrewMember=6,
+        RecalculatePlayerCrewModifiers=7,
+    }
+}
