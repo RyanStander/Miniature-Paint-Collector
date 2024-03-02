610dd1c759a7dbeb01890dd9064d7edab26d594b
Ryan <stander.ryan@gmail.com>
2024-03-02 19:34:45 +0100

    Intergration of play game successful (partially)
    Need to await approval for identify verification but after that can perform internal testing for the application.

---
+++ Assets/Scripts/GooglePlayGames/PlayGamesManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using TMPro;
+
+namespace GooglePlayGames
+{
+    public static class PlayGamesManager
+    {
+        public static void SignIn()
+        {
+            PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+        }
+
+        internal static void ProcessAuthentication(SignInStatus status)
+        {
+            if (status == SignInStatus.Success)
+            {
+                // Continue with Play Games Services
+
+                string name = PlayGamesPlatform.Instance.GetUserDisplayName();
+                string id = PlayGamesPlatform.Instance.GetUserId();
+                string imgurl = PlayGamesPlatform.Instance.GetUserImageUrl();
+            }
+            else
+            {
+                // Disable your integration with Play Games Services or show a login button
+                // to ask users to sign-in. Clicking it should call
+                // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+            }
+        }
+    }
+}
